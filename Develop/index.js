cb74b4c2d497a3b4ade96d7081a6b7635c20c2a4
Ann Markham <ann.markham@outlook.com>
2020-11-20 10:45:08 -0700

    update

--- Develop/index.js
+++ Develop/index.js
@@ -1,7 +1,63 @@
-// array of questions for user
-const questions = [
+"use strict"
+const inquirer = require('inquirer');
+const generateMarkdown = require('./utils/gererateMarkdown.js')
 
-];
+const questions = () => {
+  return inquirer.prompt([
+    {
+      type: 'input',
+      name: 'title',
+      message: 'What is the title of your project?'
+    },
+    {
+      type: 'input',
+      name: 'description',
+      message: 'Please provide a description of your project.'
+    },
+    {
+      type: 'input',
+      name: 'installation-instructions',
+      message: 'Please provide installation instructions.'
+    },
+    {
+      type: 'input',
+      name: 'usage',
+      message: 'Please provide usage instructions.'
+    },
+    {
+      type: 'checkbox',
+      name: 'license',
+      message: 'Please select the applicable license.',
+      choices: ['ADD1', 'ADD2', 'ADD3']
+    },
+    {
+      type: 'input',
+      name: 'contributors',
+      message: 'Please enter contribution guidelines for your project.  '
+    },
+    {
+      type: 'input',
+      name: 'tests',
+      message: 'Please enter test instructions.'
+    },
+    {
+      type: 'input',
+      name: 'github',
+      message: 'Please enter your GitHub Username.'
+    },
+    {
+      type: 'input',
+      name: 'link',
+      message: 'Enter the github link for your project. '
+    },
+    {
+      type: 'input',
+      name: 'contactMe',
+      message: 'Please provide email address and any additonal instructions for contacting you.'
+    },
+  ])
+
+}
 
 // function to write README file
 function writeToFile(fileName, data) {
