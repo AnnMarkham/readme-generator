ed341698e7f4ded64bb7ac24cbcc845cbe976ca5
Ann Markham <ann.markham@outlook.com>
2020-11-20 13:51:33 -0700

    update to array followed by function for output clarity

--- README.md
+++ README.md
@@ -1,3 +1,35 @@
-# Professional README Generator Starter Code
+# Your Project Title
 
-[How to create a Professional README](./readme-guide.md)
+## Badges
+
+![badmath](https://img.shields.io/github/languages/top/nielsenjared/badmath)
+
+Badges aren't _necessary_, per se, but they demonstrate street cred. Badges let other developers know that you know what you're doing. Check out the badges hosted by [shields.io](https://shields.io/). You may not understand what they all represent now, but you will in time.
+
+## Description 
+
+## Table of Contents 
+
+* [Installation](#installation)
+* [Usage](#usage)
+* [Credits](#credits)
+* [License](#license)
+
+## Installation
+
+## Usage 
+
+## Credits
+
+## License
+
+The last section of a good README is a license. This lets other developers know what they can and cannot do with your project. If you need help choosing a license, use [https://choosealicense.com/](https://choosealicense.com/)
+
+
+## Contributing
+
+If you created an application or package and would like other developers to contribute it, you will want to add guidelines for how to do so. The [Contributor Covenant](https://www.contributor-covenant.org/) is an industry standard, but you can always write your own.
+
+## Tests
+
+Go the extra mile and write tests for your application. Then provide examples on how to run them.
--- index.js
+++ index.js
@@ -1,73 +1,81 @@
 "use strict"
 const inquirer = require('inquirer');
-const generateMarkdown = require('./utils/gererateMarkdown.js')
+// const generateMarkdown = require('./utils/gererateMarkdown.js')
 
-const questions = () => {
-  return inquirer.prompt([
-    {
-      type: 'input',
-      name: 'title',
-      message: 'What is the title of your project?'
-    },
-    {
-      type: 'input',
-      name: 'description',
-      message: 'Please provide a description of your project.'
-    },
-    {
-      type: 'input',
-      name: 'installation-instructions',
-      message: 'Please provide installation instructions.'
-    },
-    {
-      type: 'input',
-      name: 'usage',
-      message: 'Please provide usage instructions.'
-    },
-    {
-      type: 'checkbox',
-      name: 'license',
-      message: 'Please select the applicable license.',
-      choices: ['ADD1', 'ADD2', 'ADD3']
-    },
-    {
-      type: 'input',
-      name: 'contributors',
-      message: 'Please enter contribution guidelines for your project.  '
-    },
-    {
-      type: 'input',
-      name: 'tests',
-      message: 'Please enter test instructions.'
-    },
-    {
-      type: 'input',
-      name: 'github',
-      message: 'Please enter your GitHub Username.'
-    },
-    {
-      type: 'input',
-      name: 'link',
-      message: 'Enter the github link for your project. '
-    },
-    {
-      type: 'input',
-      name: 'contactMe',
-      message: 'Please provide email address and any additonal instructions for contacting you.'
-    },
-  ])
+const questions = [
 
-}
-questions();
+  {
+    type: 'input',
+    name: 'title',
+    message: 'What is the title of your project?'
+  },
+  {
+    type: 'input',
+    name: 'description',
+    message: 'Please provide a description of your project.'
+  },
+  {
+    type: 'input',
+    name: 'installation-instructions',
+    message: 'What are the steps required to install your project? Provide a step-by-step description of how to get the development environment running.'
+  },
+  {
+    type: 'input',
+    name: 'usage',
+    message: 'Provide instructions and examples for use.'
+  },
+  {
+    type: 'input',
+    name: 'credits',
+    message: 'List colaborators(if any) with links to their GitHub profiles. Also list any 3rd party assets that require attribution(include creator(s) and links to primary web presence.'
+  },
+  {
+    type: 'checkbox',
+    name: 'license',
+    message: 'Please select the applicable license.',
+    choices: ['ADD1', 'ADD2', 'ADD3']
+  },
+  {
+    type: 'input',
+    name: 'contributors',
+    message: 'Please enter contribution guidelines for your project.  '
+  },
+  {
+    type: 'input',
+    name: 'tests',
+    message: 'Please enter test instructions.'
+  },
+  {
+    type: 'input',
+    name: 'github',
+    message: 'Please enter your GitHub Username.'
+  },
+  {
+    type: 'input',
+    name: 'link',
+    message: 'Enter the github link for your project. '
+  },
+  {
+    type: 'input',
+    name: 'contactMe',
+    message: 'Please provide email address and any additonal instructions for contacting you.'
+  }
+]
 
-// function to write README file
-function writeToFile(fileName, data) {
-}
+const promptUser = () => {
+  return inquirer.prompt(questions);
+};
 
-// function to initialize program
-function init() {
+promptUser();
 
-}
+// // function to write README file
+// function writeToFile(fileName, data) {
+// }
 
-// function call to initialize program
-init();
+// // function to initialize program
+// function init() {
+
+// }
+
+// // function call to initialize program
+// init();
